f341ee8281ae9a202c8ee2e08e225ce1464c832e
yerayjusto <yerayjusto@gmail.com>
2022-08-01 21:51:02 +0100

    mainpage movies responsive done

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import Movie from './Movie/Movie';
 
 const Movies = ({ movies }) => {
 
-  return (
-    <div className="row">
-    {movies.map((movie) => (
-      <h3 key={movie.id}>{movie.title}</h3>
-    ))}
-  </div>
-  )
+    return (
+        // Show all movies from api
+        <div className="row">
+            {movies.map((movie) => (
+                <Movie key={movie.id} movie={movie} />
+            ))}
+        </div>
+    );
 }
 
 export default Movies
